594e0e61c7907102e5279a6d3d4f1e332e21f078
unknown <forclass021117@gmail.com>
2022-10-15 14:01:23 +0800

    [New] add yt_download gui

--- yt_download/gui.py
+++ yt_download/gui.py
@@ -3,28 +3,33 @@ import tkinter as tk
 import time
 import threading
 import random
+from pytube_usage import str_to_ytlink, download_list
 
 class MyFrame(tk.Frame):
     def __init__(self, root):
-        tk.Frame.__init__(self, root, bg='#09c', background="red")
-        self.l1 = tk.Label(self, text="輸入文章:",width=10,relief='solid', borderwidth=1)
+        tk.Frame.__init__(self, root, background="red")
+        self.l1 = tk.Label(self, text="輸入網址:", relief='solid', borderwidth=1)
         self.l1.pack(expand=True, fill=tk.BOTH, padx=1, pady=1)
 
         self.t1 = tk.Text(self, width=5)
         self.t1.pack(expand=True, fill=tk.BOTH)
 
-        self.b1 = tk.Button(self, text="關鍵詞分析", command=self.analyse)
+        self.b1 = tk.Button(self, text="下載YT", command=self.analyse)
         self.b1.pack(expand=True, fill=tk.BOTH, pady=10)
 
-        self.result = tk.Label(self, text="點擊上面分析")
-        self.result.pack(expand=True, fill=tk.BOTH)
+        # self.result = tk.Label(self, text="點擊上面分析")
+        # self.result.pack(expand=True, fill=tk.BOTH)
 
     def analyse(self):
         def work():
             self.b1["state"] = tk.DISABLED
             time.sleep(1)
             text = self.t1.get("1.0", "end")
-            self.result["text"] = str(random.sample(range(20), k=6))
+            
+            path = r'.\music'
+            yt_list = str_to_ytlink(text)
+            download_list(yt_list, path, True)
+            
             self.b1["state"] = tk.ACTIVE
         thread = threading.Thread(target=work)
         thread.start()
@@ -33,14 +38,7 @@ if __name__=='__main__':
     window = tk.Tk()
     window.geometry("500x500+200+200")
     
-    # e = 創造元件(父親) -> e.排版()
-    # 1. 先畫Frame, 再放元件
-    # 2. 排版(pack[左右上下]/grid[表格]/absolute[絕對])
-    # 建議pack
     f1 = MyFrame(window)
     f1.pack(side=tk.LEFT, expand=True, fill=tk.BOTH, padx=20, pady=20)
-    
-    f2 = MyFrame(window)
-    f2.pack(side=tk.LEFT, expand=True, fill=tk.BOTH, padx=20, pady=20)
     
     window.mainloop()
---
+++ yt_download/pytube_usage.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+from pytube import YouTube
+from pytube import Playlist
+import time
+
+"""
+當前版本: https://pypi.org/project/pytube/
+pytube 12.1.0
+"""
+
+def download_list(yt_list, path = './', audio_only=False):
+    """
+    函數功能: 
+    yt_list: list of YouTube
+    path: 下載影片路徑，未指定則載至當前資料夾
+    audio_only: 是否僅下載聲音檔
+    """
+    if not os.path.isdir(path):  #防呆: 如果資料夾不存在就建立
+        os.mkdir(path)
+    for i, yt in enumerate(yt_list):
+        try:
+            print(f"{i}. {yt.title}")  #顯示標題
+            yt_fi = yt.streams.filter()
+            loader = yt_fi.get_highest_resolution() if not audio_only else yt_fi.get_audio_only()
+            ext = 'mp4' if not audio_only else 'mp3'
+            name = re.sub('[:/]','',yt.title) # 去除特殊字元
+            loader.download(output_path=path, filename=f'{name}.{ext}')
+        except Exception as e:
+            print(f'第{i}個影片下載失敗: {e}')
+        time.sleep(2) # 避免連續發太多request給線上
+
+def url_to_ytlink(url):
+    """
+    將yt網址轉換為可下載的yt清單
+    (sample: https://www.youtube.com/watch?v=mdSXKdnLX9I&list=PLMYdDQxtZ9cHn98uzc5V9JdaKcmrkuRSC&index=1)
+    """
+    return [y for y in Playlist(url).videos]
+
+def txt_to_ytlink(file):
+    """
+    將檔案中的yt網址(以換行符號分隔)轉換為可下載的yt清單
+    """
+    with open(file) as file:
+        urls = file.readlines()
+    return [YouTube(line) for line in urls]
+
+def str_to_ytlink(s):
+    """
+    將字串(以換行符號分隔)轉換為可下載的yt清單
+    """
+    urls = s.split('\n')
+    return [YouTube(line) for line in urls if line]
+
+        
+if __name__ == "__main__":
+    path = r'.\music'
+    yt_list = txt_to_ytlink('video.txt')
+    download_list(yt_list, path, True)
